c879ee5ab8f127f29dc24a65bed5c19a22abb74f
Yangira134 <134039003+Yangira134@users.noreply.github.com>
2023-05-26 08:45:57 +0300

    проект

--- Lab1/src/Lab1.java
+++ Lab1/src/Lab1.java
@@ -1,6 +1,6 @@
 public class Lab1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Respawn");
+        System.out.println("hello home");
     }
 }
